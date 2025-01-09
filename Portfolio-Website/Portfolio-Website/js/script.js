d66f545c6966dd0a3bb7575f00e4d19e604a2b6e
Connor Shen <connor.lishen@gmail.com>
2025-01-09 05:28:29 -0600

    fixed the wording

--- Portfolio-Website/Portfolio-Website/js/script.js
+++ Portfolio-Website/Portfolio-Website/js/script.js
@@ -27,7 +27,7 @@ window.addEventListener('load', () => {
             setTimeout(() => {
                 line1.classList.remove('fade-out'); // Remove fade-out class
                 line1.style.opacity = "1"; // Reset opacity for new text
-                typeText(line1, "My name is Connor and", 1500); // Type "My name is Connor" in the same position
+                typeText(line1, "My name is Connor", 1500); // Type "My name is Connor" in the same position
             }, 3000); // Wait for fade-out to complete before typing next line
         }, 1000); // Pause for 1 second after typing "Hey!"
     }, 1000); // Delay before the pause and fade-out logic
