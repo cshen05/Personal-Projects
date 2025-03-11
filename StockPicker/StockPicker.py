c25fae9a1e40b89e440d4f50dab88a89fa9bd6d0
Connor Shen <connor.lishen@gmail.com>
2025-03-11 15:07:05 -0500

    fixed minor bug

--- StockPicker/StockPicker.py
+++ StockPicker/StockPicker.py
@@ -493,7 +493,7 @@ def initialize_trading_system(status_var, settings, ticker_csv=None):
             tickers = []
     else:
         try:
-            tickers = pd.read_csv('nyse-listed.csv')['ACT Symbol'].tolist()
+            tickers = pd.read_csv('test.csv')['ACT Symbol'].tolist()
         except Exception as e:
             send_alert(f"Error loading default ticker CSV: {str(e)}")
             tickers = []
